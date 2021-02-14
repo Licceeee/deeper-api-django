c70b970770f1f83505873908c8937abdb96f3e75
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-02-14 01:14:15 +0100

    core app added + central template folder

--- deeper/config/settings.py
+++ deeper/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "graphene_django",
+    'graphene_django',
+    'core.apps.CoreConfig',
     'card.apps.CardConfig',
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
