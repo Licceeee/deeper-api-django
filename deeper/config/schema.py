06e6d1d99993b9acec56650083851d256c70c7cb
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-07-20 09:27:17 +0200

    update schema indent

--- deeper/config/schema.py
+++ deeper/config/schema.py
@@ -12,31 +12,28 @@ class CategoryNode(DjangoObjectType):
 class QuestionNode(DjangoObjectType):
     class Meta:
         model = Question
-        
+
 
 class Query(graphene.ObjectType):
     categories = graphene.List(CategoryNode)
     category_by_id = graphene.Field(CategoryNode, id=graphene.Int())
-    
+
     questions = graphene.List(QuestionNode)
     question_by_id = graphene.Field(QuestionNode, id=graphene.Int())
 
-
     def resolve_categories(self, info, **kwargs):
         return Category.objects.all()
-    
+
     def resolve_category_by_id(self, info, id):
         # Querying a single Category
         return Category.objects.get(pk=id)
 
-
     def resolve_questions(self, info, **kwargs):
         return Question.objects.all()
-    
+
     def resolve_question_by_id(self, info, id):
         # Querying a single question
         return Question.objects.get(pk=id)
-
 
 
 schema = graphene.Schema(query=Query)
